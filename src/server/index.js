95ddc3aa18faa13c43b9c0f00980f20887c8d848
Islam-cod <74316365+Islam-cod@users.noreply.github.com>
2022-04-25 03:04:41 +0200

    config files finished

--- src/server/index.js
+++ src/server/index.js
@@ -2,24 +2,40 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 //API credentials
-var textapi = new aylien({
-    application_key: process.env.API_KEY
-  });
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
+const textApi = process.env.API_KEY
+let userInput = []
 
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch');
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
-
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
+
+// A Post route
+app.post('/api', async function(req, res) {
+    userInput = req.body.url;
+    console.log(`You entered: ${userInput}`);
+    const apiURL = `${baseURL}key=${textApi}&url=${userInput}&lang=en`
+
+    const response = await fetch(apiURL)
+    const data = await response.json()
+    console.log(data)
+    res.send(data) })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
