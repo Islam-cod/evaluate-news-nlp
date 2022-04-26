3dd1386e5c2c9d9e5a7747ac08b090a34b418419
Islam-cod <74316365+Islam-cod@users.noreply.github.com>
2022-04-26 03:07:42 +0200

    project finalized

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,9 +3,20 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
 
-    console.log("::: Form Submitted :::")
+    if(Client.validateURL(formText)) {
+        console.log("::: Form Submitted :::")
+        postData('http://localhost:8080/api', {url: formText})
+        .then(function(res) {
+            document.getElementById('polarity').innerHTML = 'Polarity: '+ polarityChecker(res.score_tag);
+            document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`;
+            document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+            document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
+            document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
+        })
+        } else {
+            alert('invalid URL');
+        }
     fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
@@ -13,4 +24,51 @@ function handleSubmit(event) {
     })
 }
 
+
+//API response as polarity score
+const polarityChecker = (score) => {
+    let display;
+    switch (score){
+        case 'P+':
+            display = 'strong positive';
+            break;
+        case 'P':
+            display = 'positive';
+            break;
+        case 'NEW':
+            display = 'neutral';
+            break;
+        case 'N':
+            display = 'negative';
+            break;
+        case 'N+':
+            display = 'strong negative';
+            break;
+        case 'NONE':
+            display = 'no sentiment';
+    }
+    return display.toUpperCase();
+}
+
+//Post request
+const postData = async (url = "", data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const resData = await response.json();
+        console.log('Data received:', resData)
+        return resData;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
 export { handleSubmit }
+export { polarityChecker }
